93fb38c8edd3b6c6d05206b8406b2fe8a0bafadd
Luiz Henrique Bezerra <lhbezerra@hotmail.com>
2015-11-27 01:56:59 -0200

    video

---
+++ app/common/youtube.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	youtubeParser: function(url){  
+	    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+	    var match = url.match(regExp);
+	    return (match&&match[7].length==11)? match[7] : false;
+	}
+	
+}
